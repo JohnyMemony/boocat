b711ce75ec13ffba9fad2579b351d583ad80aa2a
Gennadiy Yatsenko <lKpOJIuK@gmail.com>
2020-01-06 10:40:25 +0200

    Add initial project template

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Router from './Router';
+
+import './assets/styles/main.scss';
+
+ReactDom.render(
+  <Router />,
+  document.getElementById('root'),
+);
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/App.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'main.bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [require('autoprefixer')]
+            },
+          },
+          'sass-loader',
+        ]
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './src/index.html' }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+  devServer: {
+    port: 1000,
+    compress: true,
+    overlay: true,
+  },
+};
