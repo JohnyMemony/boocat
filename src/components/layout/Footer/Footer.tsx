faa7755936a3e376da258adf809a20ec79cf2df8
Gennadiy Yatsenko <lkpojiuk@gmail.com>
2020-03-03 23:38:00 +0200

    Add footer component

---
+++ src/components/layout/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+import './Footer.scss';
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="footer__copyright">Boocat © 2020</div>
+      </div>
+    </footer>
+  )
+}
--- src/components/layout/RootLayout/RootLayout.tsx
+++ src/components/layout/RootLayout/RootLayout.tsx
@@ -1,13 +1,17 @@
 import * as React from 'react';
 import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 
 import './RootLayout.scss';
 
-export default function RootLayout({children}: {children: React.ReactNode}) {
+export default function RootLayout({children}: { children: React.ReactNode }) {
   return (
-    <div className="bc-page">
-      <Header />
-      {children}
-    </div>
+    <>
+      <div className="bc-page">
+        <Header/>
+        {children}
+      </div>
+      <Footer/>
+    </>
   );
 }
